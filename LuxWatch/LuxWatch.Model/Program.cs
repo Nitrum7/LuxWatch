825c0f9c3f2ca60815b7c11d35ce341facdf8db6
AmgoNiko <gagov2004@abv.bg>
2022-03-12 12:58:34 +0200

    Update

---
+++ LuxWatch/LuxWatch.Model/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LuxWatch.Model
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ LuxWatch/LuxWatch.Service/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LuxWatch.Service
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
